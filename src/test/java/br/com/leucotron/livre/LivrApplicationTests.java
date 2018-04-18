fda92e2283b5b8387721d0b83851a0ce43f079d5
Caio Fabio Marinho Barbosa <caio.barbosa@virtus.ufcg.edu.br>
2018-04-18 11:12:43 -0300

    Insert new tests to organization and delete flow repository, flow service and flowdto

--- src/test/java/br/com/leucotron/livre/LivrApplicationTests.java
+++ src/test/java/br/com/leucotron/livre/LivrApplicationTests.java
@@ -2,18 +2,21 @@ package br.com.leucotron.livre;
 
 import br.com.leucotron.livre.util.FunctionalTest;
 import io.restassured.http.ContentType;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.json.JSONException;
-import org.json.JSONObject;
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
-import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+import static io.restassured.RestAssured.given;
 
 @RunWith(SpringRunner.class)
 public class LivrApplicationTests extends FunctionalTest {
+
+    @Test
+    public void applicationContextLoaded() {
+
+    }
 
     @Test
     public void loginReturn200() throws JSONException {
