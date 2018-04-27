fcbfab0f3278c44a11d16169fa60ee49deedd36f
Caio Fabio Marinho Barbosa <caio.barbosa@virtus.ufcg.edu.br>
2018-04-27 14:14:10 -0300

    update sql file init

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -3,7 +3,7 @@ CREATE TABLE `user` (
   `name` varchar(255) NOT NULL,
   `login` varchar(255) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `tags` text NOT NULL,
+  `tags` text,
   PRIMARY KEY (`iduser`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
@@ -12,6 +12,6 @@ CREATE TABLE `organization` (
   `idorganization` integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `status` bool NOT NULL,
-  `tags` text NOT NULL,
+  `tags` text,
   `accesskey` varchar(255) NOT NULL
 )ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8;
