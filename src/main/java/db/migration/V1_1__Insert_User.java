ee0450561a946b0825a78053aac695282796c180
Caio Fabio Marinho Barbosa <caio.barbosa@virtus.ufcg.edu.br>
2018-05-16 16:34:06 -0300

    fix keys in sql insert

--- src/main/java/db/migration/V1_1__Insert_User.java
+++ src/main/java/db/migration/V1_1__Insert_User.java
@@ -10,12 +10,12 @@ public class V1_1__Insert_User implements SpringJdbcMigration {
     public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
         jdbcTemplate.execute("\n" +
                 "INSERT INTO livre.user (name,login,password,tags,role,master) VALUES\n" +
-                "\t('Administrador','admin@leucotron.com.br','��uEjn��R �����','', 'super_user', '1');");
+                "\t('Administrador','admin@leucotron.com.br','��uEjn��R �����','', 'SUPER_USER', '1');");
         jdbcTemplate.execute("\n" +
                 "INSERT INTO livre.role (role) VALUES\n" +
-                "\t('super_user');");
+                "\t('SUPER_USER');");
         jdbcTemplate.execute("\n" +
                 "INSERT INTO livre.role (role) VALUES\n" +
-                "\t('user');");
+                "\t('USER');");
     }
 }
