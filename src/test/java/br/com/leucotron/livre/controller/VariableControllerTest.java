4276c880b6f742a55ad874e7b7bc04f88a703898
Caio Fabio Marinho Barbosa <caio.barbosa@virtus.ufcg.edu.br>
2018-05-28 15:09:36 -0300

    Fix tests

--- src/test/java/br/com/leucotron/livre/controller/VariableControllerTest.java
+++ src/test/java/br/com/leucotron/livre/controller/VariableControllerTest.java
@@ -55,7 +55,8 @@ public class VariableControllerTest extends FunctionalTest {
     public void createVariableAndReturn201() throws JSONException {
 
         Project project = createProject();
-
+        System.out.println("project id: " + project.getIdProject());
+        System.out.println("org id: " + project.getOrganization().getIdOrganization());
         this.getAuthRestAssured()
                 .contentType(ContentType.JSON)
                 .body(createVariableJson(project.getIdProject(), project.getOrganization().getIdOrganization()).toString())
@@ -78,6 +79,8 @@ public class VariableControllerTest extends FunctionalTest {
         variable.setTags(VARIABLE_UPDATE_TAGS);
 
         VariableDTO pd = MapperUtil.mapTo(variable, VariableDTO.class);
+        pd.setIdOrganization(variable.getProject().getOrganization().getIdOrganization());
+        System.out.println("*************************************");
         System.out.println(pd.getIdOrganization());
         this.getAuthRestAssured()
                 .contentType(ContentType.JSON)
